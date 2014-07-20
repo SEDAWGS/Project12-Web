8084ad56f8b7a06b6e455a5be1024afbe6be1467
rageandqq <chitley.sameer@gmail.com>
2014-07-20 07:46:16 -0400

    Added Cloud Code

---
+++ CloudCode/cloud/main.js
@@ -0,0 +1,12 @@
+Parse.Cloud.define("querySublets", function(request, response) {
+	var query = new Parse.Query("SLObject");
+	query.equalTo("tags", request.params.query);
+	query.find({
+		success: function(resuts) {
+			response.success(results);
+		},
+		error: function() {
+			response.error("No Objects found,");
+		}
+	});
+});
