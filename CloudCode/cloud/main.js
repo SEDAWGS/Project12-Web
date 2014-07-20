2ca0b2f1d2ea804a7a368a9232cf9d755fb6210c
rageandqq <chitley.sameer@gmail.com>
2014-07-20 08:43:12 -0400

    Made tags lower case

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,6 +1,10 @@
 Parse.Cloud.define("querySublets", function(request, response) {
 	var query = new Parse.Query("SLObject");
-	query.equalTo("tags", request.params.query);
+	var searchQuery = request.params.query;
+	if (serachQuery == null || searchQuery == '') {
+		response.error("No Search Defined");
+	}
+	query.equalTo("tags", searchQuery.toLowerCase());
 	query.find({
 		success: function(results) {
 			response.success(results);
